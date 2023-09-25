7e423646a633977840483aec94f3f10de5b805de
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2023-09-25 20:01:39 +0530

    Create sp5.c

---
+++ Semester 1/Lab 1/SP/sp5.c
@@ -0,0 +1,11 @@
+﻿
+
+#include <stdio.h>
+void main(){
+    float len, bred;
+    printf("Enter the length & width of the rectangle: ");
+    scanf("%f %f",&len, &bred);
+    float area = len*bred;
+    float peri = 2.0* (len+bred);
+    printf("The Perimeter and area of rectangle = %f, %f units \n", peri, area);
+}
