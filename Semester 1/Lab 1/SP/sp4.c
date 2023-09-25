9443282f2edaf1fd43410bdefaef473e0c1f2b2f
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2023-09-25 19:59:57 +0530

    Create sp4.c

---
+++ Semester 1/Lab 1/SP/sp4.c
@@ -0,0 +1,10 @@
+﻿
+
+#include <stdio.h>
+void main(){
+  float p,t,r;
+  printf("Enter principal, rate and time: "); 
+  scanf("%f %f %f",&p,&t,&r);
+  float si = (p*t*r)/100;
+  printf("Simple Interest = %f\n",si);
+}
