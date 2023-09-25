94ea30545457ceb7fed7df5634de2f392c5ae116
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2023-09-25 19:57:13 +0530

    Create sp3.c

---
+++ Semester 1/Lab 1/SP/sp3.c
@@ -0,0 +1,16 @@
+﻿
+
+#include <stdio.h> 
+int main(){ 
+  char a;
+  int b; 
+  float c;
+  printf("Enter a character: ");
+  scanf("%c", &a);
+  printf("Enter the integer: ");
+  scanf("%d", &b);
+  printf("Enter the float value: ");
+  scanf("%f",&c);
+  printf("Character: %c\n",a);
+  printf("Integer value: %d\n",b); printf("Float value: %f\n",c);
+}
